08e297a883aad78bdb2872b7e84c727791f9d8b0
h_horiike <h_horiike@sbs-infosys.co.jp>
2021-11-29 09:34:57 +0900

    スタイル微修正

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,16 @@
 import React from "react";
 import map from "../map.png";
 
-const Map = () => <img src={map} alt="test" />;
+const Map = () => (
+  <img
+    src={map}
+    alt="test"
+    style={{
+      objectFit: "cover",
+      width: "100%",
+      height: "100%",
+    }}
+  />
+);
 
 export default Map;
